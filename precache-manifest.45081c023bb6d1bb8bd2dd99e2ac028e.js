1d55da5420ac2e4e557cc2ce4bb9b7f249cb9b1b
unknown <kebcdy@gmail.com>
2020-01-04 14:51:27 +0800

    Updates

---
+++ precache-manifest.45081c023bb6d1bb8bd2dd99e2ac028e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb4211911658a1d7aa5ec1c1746d1b10",
+    "url": "/gobang/index.html"
+  },
+  {
+    "revision": "93e2fd3f61ff202fc3d8",
+    "url": "/gobang/static/css/main.8e6c0635.chunk.css"
+  },
+  {
+    "revision": "1cf97b65444ea63902fd",
+    "url": "/gobang/static/js/2.635a2bc7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/gobang/static/js/2.635a2bc7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "93e2fd3f61ff202fc3d8",
+    "url": "/gobang/static/js/main.e7cd38c8.chunk.js"
+  },
+  {
+    "revision": "fda097e64cee1ed4fdad",
+    "url": "/gobang/static/js/runtime-main.9bc7ba5c.js"
+  }
+]);
